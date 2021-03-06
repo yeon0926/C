cec130eb56a838b8ab3cb8d021772d4fdf16dcc4
hayeon <57000168+yeon0926@users.noreply.github.com>
2021-03-06 15:09:11 +0900

    Add files via upload

---
+++ program/2_6.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+main()
+{
+	/* \t는 수평 탭, \n은 개행 */
+	printf("%s\t%s\n", "Linux", "Programing");
+	/* \'는 작은 따옴표 */
+	printf("%s\'s name\n", "Kim");
+}
---
+++ program/3_3.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+main()
+{
+	int a=5, b, c, d;
+	/* a가 7보다 큰지 비교 */
+	b=a<7;
+	printf("b=%d\n", b);
+	/* a가 5와 같은지 비교 */
+	d=a==5;
+	printf("d=%d\n", d);
+}
